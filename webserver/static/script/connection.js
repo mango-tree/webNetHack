b2e9f83ed6569d5f25f7671213a3c83ecd796d1d
mango-tree <k.mingyu.95@gmail.com>
2017-08-06 17:31:13 +0900

    imp: remove JQuery library and modify JS code (#15)

--- webserver/static/script/connection.js
+++ webserver/static/script/connection.js
@@ -1,64 +1,45 @@
 var host = "ws://mango-tree.xyz:8000";
-//var vt100 = require('./contrib/vt100/vt100')
-//var helpers = require('./helpers/all')
 
-//var display = document.getElementById('display');//$("#display");
-//var vt100 = new VT100({canvas:document.getElementById('display')})
-
-//var v = new VT100({
-//    size: {x: 20, y: 20},
-    //display: (new helpers.Display)
-//})
-var controller=display.getContext("2d");
 connect();
 
-
-function connect(){
+function connect() {
     var socket;
     try {
         var socket = new WebSocket(host);
-        socket.onopen = function(){  
-           alert("Socket has been opened!"); 
-        }  
-        socket.onmessage = function(msg){  
+        socket.onopen = function () {
+            alert("Socket has been opened!");
+        }
+        socket.onmessage = function (msg) {
             parseMsg(msg);
-           //alert(msg.data); //Awesome!  
-           html = AnsiUp.prototype.ansi_to_html(msg.data);
-           controller.fillText(html,10,10);
-
-           $("#palatte").append(msg.data);
-           //v.write(msg.data);
-            
-        }  
-    } catch(exception) {
+            document.querySelector("#palatte").innerHTML += msg.data;
+        }
+    } catch (exception) {
         message(exception);
     }
 
     function send_data(text) {
         try {
             socket.send(text);
-        } catch(exception) {
+        } catch (exception) {
             message(exception);
         }
     }
 
-    $(document).keypress(function(event) {  
+    document.addEventListener("keypress", function (event) {
         var charcode = getChar(event);
         send_data(charcode);
     });
 }
+
 function getChar(event) {
     event = event || window.event;
     var keyCode = event.which || event.keyCode;
     var typedChar = String.fromCharCode(keyCode);
-    console.log('press: '+typedChar);
+    console.log('press: ' + typedChar);
     return typedChar;
 }
-function parseMsg(msg)
-{
-    
+
+function parseMsg(msg) {
     console.log(msg);
 }
-
-
 
